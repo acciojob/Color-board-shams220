736d3159f3e0e80d8127ee602e2f3835c48d6d13
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-25 11:26:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-//your JS code here. If required.
+
+
+const mainDiv = document.querySelector(".container");
+
+mainDiv.style.backgroundColor = "black";
+mainDiv.style.width=  "100vw";
+mainDiv.style.height = "100vh";
+      mainDiv.style.margin = "0"; // removes unwanted spacing
+
+
+  const  randomRGB=()=> {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+for(let i = 1;i<=800;i++){
+	const div = document.createElement("div");
+	div.classList.add("square");
+	div.style.width = "14px";
+	div.style.height = "14px";
+    div.style.margin ="4px"
+	div.style.backgroundColor = "grey";
+	document.querySelector(".container").appendChild(div);
+}
+
+
+
+const sq = document.querySelectorAll('.square');
+sq.forEach(s=>
+s.addEventListener('mouseover',()=>{
+    let color= randomRGB()
+   
+    s.style.backgroundColor=color;
+})
+
+
+)
+
+sq.forEach(s=>
+s.addEventListener('mouseout',(e)=>{
+ 
+    
+    s.style.backgroundColor="grey";
+})
+
+
+)
+
